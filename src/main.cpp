2d7a5625a0c0fdc71af8c3c4097a14d3e9446c44
Grafscorp <golyandringraf@gmail.com>
2025-05-09 14:08:58 +0300

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+
+#define KEYPIN1  //TODO
+#define KEYPIN2  //TODO
+#define KEYPIN3  //TODO
+#define KEYPIN4  //TODO
+
+#define ENCODERPIN //TODO
+
+
+
+void setup() {
+  //BIND PINS
+  
+}             
+ 
+void loop() {
+
+}
+
